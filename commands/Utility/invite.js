0524550f7edbc6035a8cffae9c76c0f46c1b08e2
Koala <73330704+olykoala@users.noreply.github.com>
2022-11-02 22:45:00 +1100

    Update invite.js

--- commands/Utility/invite.js
+++ commands/Utility/invite.js
@@ -12,7 +12,7 @@ module.exports = {
        var embed=new MessageEmbed()
        .setTitle(`Invite Watchout`)
        .setColor('#FFFFFF')
-       .setDescription('[Click here](https://discord.com/api/oauth2/authorize?client_id=930079200428318730&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2FXSrrWTEr3A&response_type=code&scope=bot%20applications.commands%20identify) to invite Watchout to your server. Note a requirement of at least 2K members is required for Watchout to be used inside your server.')
+       .setDescription('[Click here](https://discord.com/api/oauth2/authorize?client_id=1036805804361269259&permissions=8&scope=bot%20applications.commands) to invite Watchout to your server. Note a requirement of at least 2K members is required for Watchout to be used inside your server.')
        message.reply({embeds: [embed]})
     },
   };
