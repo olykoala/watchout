773099e05831e8be2a9e2e82d2d949da59a4e30a
Koala <73330704+olykoala@users.noreply.github.com>
2022-11-02 22:30:17 +1100

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -16,7 +16,7 @@ client.on("messageCreate", async (message) => {
 client.on('guildCreate', async guild => {
   const members = guild.members.cache.filter(member => !member.user.bot);
 const bots = guild.members.cache.filter(member => member.user.bot);
- // if(members.size < 2000) return guild.leave()
+  if(members.size < 2000) return guild.leave()
 	
 })
 
@@ -39,32 +39,9 @@ client.on("messageDelete", async (message) => {
   modChannel.send({content: `**${message.author.tag}:** (\`${message.id}\`) ${message.content}\nMessage ID: ${message.id} Channel: ${message.channel}`, components: [row]})
      })
 
-var fs = require('fs')
-var logger = fs.createWriteStream('logs/message-log.txt', {
-  flags: 'a'
-})
-
-client.on("messageCreate", async (message) => {
-  if(message.author.bot) {
-  var writeLine = (line) => logger.write(`\n${line}`);
-writeLine(`\n${message.author.tag} (${message.author.id}): ${message.content}`);
-  } else {
-  var writeLine = (line) => logger.write(`\n${line}`);
-    
-    if(message. attachments.size>0) return writeLine(`\n${message.author.tag} (${message.author.id}): ${message.content}\nImage Link: ${message.attachments.first().url}\nMessage ID: ${message.id} Channel: ${message.channel.name}`);
-    
-writeLine(`\n${message.author.tag} (${message.author.id}) BOT: ${message.content}\nMessage ID: ${message.id} Channel: #${message.channel.name}`);
-  }
-})
-
 client.on("messageUpdate", async (message, oldMessage, newMessage) => {
   if (!oldMessage.author) return;
   if (oldMessage.author.bot) return;
-  var writeLine = (line) => logger.write(`\n${line}`);
-  
-    if(message. attachments.size>0) return writeLine(`\nMESSAGE EDIT\n${message.author.tag} (${message.author.id})\nBEFORE: ${message.content}\nNOW: ${oldMessage}\nImage Link: ${message.attachments.first().url}\nMessage ID: ${message.id} Channel: #${message.channel.name}`);
-  
-writeLine(`\nMESSAGE EDIT\n${message.author.tag} (${message.author.id})\nBEFORE: ${message.content}\nNOW: ${oldMessage}\nMessage ID: ${message.id} Channel: #${message.channel.name}`);
      if(message.author.bot) return;
   
   let channel = db.fetch(`msglogs_${message.guild.id}`)
@@ -84,27 +61,6 @@ writeLine(`\nMESSAGE EDIT\n${message.author.tag} (${message.author.id})\nBEFORE:
   modChannel.send({content: `**${message.author.tag}** (\`${message.author.id}\`)\nBEFORE: ${message.content}\nNOW: ${oldMessage}\nMessage ID: ${message.id} Channel: ${message.channel}`, components: [row]})
 })
 
-client.on("messageCreate", async (message) => {
-  var logger3 = fs.createWriteStream(`logs/user-messages/${message.author.id}`, {
-  flags: 'a'
-})
-  var writeLine = (line) => logger3.write(`\n${line}`);
-  if(message. attachments.size>0) return writeLine(`\n${message.author.tag} (${message.author.id}): ${message.content}\nImage Link: ${message.attachments.first().url}\nMessage ID: ${message.id} Channel: #${message.channel.name}`);
-writeLine(`\n${message.author.tag} (${message.author.id}): ${message.content}\nMessage ID: ${message.id} Channel: #${message.channel.name}`);
-  })
-
-client.on("messageUpdate", async (message, oldMessage, newMessage) => {
-    var logger3 = fs.createWriteStream(`logs/user-messages/${message.author.id}`, {
-  flags: 'a'
-})
-  if (!oldMessage.author) return;
-  if (oldMessage.author.bot) return;
-  var writeLine = (line) => logger3.write(`\n${line}`);
-  
-    if(message. attachments.size>0) return writeLine(`\nMESSAGE EDIT\n${message.author.tag} (${message.author.id})\nBEFORE: ${message.content}\nNOW: ${oldMessage}\nImage Link: ${message.attachments.first().url}\nMessage ID: ${message.id} Channel: ${message.channel.name}`);
-  
-writeLine(`\nMESSAGE EDIT\n${message.author.tag} (${message.author.id})\nBEFORE: ${message.content}\nNOW: ${oldMessage}\nMessage ID: ${message.id} Channel: #${message.channel.name}`);
-})
 
 client.on("guildMemberAdd", async member => {
   var welchannel = client.channels.cache.get(`1023825167492857887`)
